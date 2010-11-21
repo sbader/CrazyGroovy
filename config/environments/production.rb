5dee3d95175475251aa5dcbb786e7e897f0252cc
dev <dev@Scott-Baders-MacBook-Pro.local>
2010-11-21 18:10:53 -0500

    static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,5 @@ Audio::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   Sass::Plugin.options[:never_update] = true
+  config.serve_static_assets = true
 end
