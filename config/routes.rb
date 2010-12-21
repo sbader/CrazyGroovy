52a229d9d9bc49079d7784254abac3f1a92b5cbc
sbader <sbader2@gmail.com>
2010-12-20 23:51:14 -0500

    added shorthand devise routes and scoped views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Audio::Application.routes.draw do
 
   resources :songs
   resources :albums
+  
+  devise_scope :user do
+    get "signin", :to => "devise/sessions#new", :as => "new_user_session"
+    get "signout", :to => "devise/sessions#destroy", :as => "destroy_user_session"
+    get "signup", :to => "devise/registrations#new", :as => "new_user_registration"
+    post "signin", :to => "devise/sessions#create", :as => "new_user_session"
+    post "signup", :to => "devise/registrations#create", :as => "new_user_registration"
+    get "forgotpassword", :to => "devise/passwords#new", :as => "new_user_password"
+    post "forgotpassword", :to => "devise/passwords#create", :as => "new_user_password"
+  end
 
   root :to => "albums#index"
 
